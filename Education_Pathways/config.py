1c8fa48bd18e377a784392579b95c7fbd1bee440
Kevin Li <kevin.li20021106@gmail.com>
2022-10-13 20:18:59 -0400

    Upload resources and modify backend

--- Education_Pathways/config.py
+++ Education_Pathways/config.py
@@ -7,15 +7,18 @@ app = None
 db = None
 cors = None
 
+
 def init_app(app_):
     global app
     app = app_
     return app
 
+
 def init_db(app):
     global db
     db = MongoEngine(app)
     return db
+
 
 def init_cors(app):
     global cors
