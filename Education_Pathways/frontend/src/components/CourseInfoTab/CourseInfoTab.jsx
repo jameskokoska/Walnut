4b990935399a831f5a33992abeaa11bee307096a
Kevin Li <kevin.li20021106@gmail.com>
2022-10-20 01:37:02 -0400

    Styling to tabs

--- Education_Pathways/frontend/src/components/CourseInfoTab/CourseInfoTab.jsx
+++ Education_Pathways/frontend/src/components/CourseInfoTab/CourseInfoTab.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./CourseInfoTab.css";
 
-export default function CourseInfoTab({ icon, text, secondary }) {
+export default function CourseInfoTab({ icon, text, active, secondary }) {
+  const bgColor = { backgroundColor: active ? "#243E6B" : "white" };
   return (
-    <div className="courseInfo-tab">
+    <div className="courseInfo-tab" style={bgColor}>
       <div className="icon-container">{icon}</div>
       <div className="text-container">{text}</div>
     </div>
