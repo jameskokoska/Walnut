528518c4ade0c6f723089b79fd4d2905f84abad0
Kevin Li <kevin.li20021106@gmail.com>
2022-10-21 19:56:52 -0400

    Retrieve data and render

--- Education_Pathways/frontend/src/pages/CourseInfoPage/CourseInfoPage.jsx
+++ Education_Pathways/frontend/src/pages/CourseInfoPage/CourseInfoPage.jsx
@@ -15,7 +15,13 @@ export default function CourseInfoPage() {
     department: "",
     course_description: "",
     prerequisites: "",
+    course_level: "",
+    campus: "",
+    terms: "",
     corequisites: "",
+    recommeded_prep: "",
+    breadth: "",
+    distribution: "",
     exclusions: "",
   });
   const navigate = useNavigate();
@@ -30,13 +36,27 @@ export default function CourseInfoPage() {
     API.get(`/course/details?code=${code}`)
       .then((res) => {
         const data = res.data.course;
+        console.log(data);
         setCourseState({
           course_code: data.code,
           course_name: data.name,
+          division: data.division,
+          department: data.department,
+          course_description: data.description,
+          prerequisites: "data.prereq",
+          course_level: data.level,
+          campus: data.campus,
+          terms: data.term,
+          corequisites: data.coreq,
+          recommeded_prep: data.prep,
+          breadth: data.br,
+          distribution: data.dist,
+          exclusions: data.exclusion,
         });
       })
-      .catch(() => {
-        navigate("/*");
+      .catch((err) => {
+        console.log(err);
+        navigate("/course-not-found");
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -49,7 +69,10 @@ export default function CourseInfoPage() {
         <CourseInfoSideBar section={section} onClick={onTabClick} />
       </div>
       <div className="courseInfo-main">
-        <div style={{ display: section[0] ? "block" : "none" }}>Info</div>
+        <div style={{ display: section[0] ? "block" : "none" }}>
+          <h1>Overview</h1>
+          <p>{courseState.course_description}</p>
+        </div>
         <div style={{ display: section[1] ? "block" : "none" }}>Review</div>
         <div style={{ display: section[2] ? "block" : "none" }}>Prof</div>
         <div style={{ display: section[3] ? "block" : "none" }}>Exam</div>
