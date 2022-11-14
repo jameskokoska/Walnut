cc7cdde82eefc81e06639ae1abb0113933eb0535
Sam Pan <sam.pan2000325@gmail.com>
2022-11-14 18:47:18 -0500

    added How to Use Walnut section to about page

--- Education_Pathways/frontend/src/pages/AboutPage.jsx
+++ Education_Pathways/frontend/src/pages/AboutPage.jsx
@@ -41,7 +41,20 @@ export default function AboutPage() {
         university (Acorn website and Chestnut Residence).<br /><br /><br /></p>
         <img src="https://firebasestorage.googleapis.com/v0/b/codeless-app.appspot.com/o/projects%2FQay1zSJfS8ysAs7pMqG6%2F065cec9d7fe456a06cb81cc0a8fe4541?alt=media&token=28abafad-0097-4124-9b26-0b6b17313f85" 
         className="img1"/>
-  
+
+
+      <h1 className="meettheteam">How to use Walnut</h1>
+      <p className="intro">
+      <br />To use Walnut, enter the course code or name in the search bar and click the search button. To view more information about a course,
+        click on the course card. If you don't have a specific course in mind, you can also browse through the course catalog by searching for a department, faculty, 
+        or other keywords and Walnut will provide you with a list of courses that match your search. 
+        <br /><br />
+        Can't decide between two similar courses? No problem! Simply click the compare button on the course card and Walnut will display the two courses side-by-side.
+        This way, you can easily compare the two courses and make an informed decision.
+        Walnut also allows you to save courses to your watchlist so that you can easily access them later. To save a course to your watchlist, click the heart button on the course card,
+        and the course will be saved to your watchlist. If you want to access your watchlist, click the watchlist button on the top right of the page, and you will be able to see all your saved courses on one page.
+        <br /><br /><br /></p>
+
       <h1 className="meettheteam">Meet The Team</h1>
       <div style = {{marginTop: "30px"}} className="team">
         {teamintro.map((member) => {
