75cf345ddcd7158a82dc25f253a239d0dbf2acbc
Kevin Li <kevin.li20021106@gmail.com>
2022-11-14 20:23:39 -0500

    Fix scroll bar bug

--- Education_Pathways/frontend/src/pages/Favorites/Favorites.jsx
+++ Education_Pathways/frontend/src/pages/Favorites/Favorites.jsx
@@ -16,23 +16,25 @@ export default function Favorites(props) {
   const favorites = getFavorites();
   return (
     <div className="favorites-page">
-      <div className="favorites-page-title">
-        <h2>Favorites</h2>
-        <h3>
-          {favorites.length === 1
-            ? `There is ${favorites.length} course saved.`
-            : `There are ${favorites.length} courses saved.`}
-        </h3>
-      </div>
-      <div className="favorites-list">
-        {favorites.map((favorite) => {
-          return (
-            <FavoriteContainer
-              courseCode={favorite}
-              setCourseCode={props.setCourseCode}
-            />
-          );
-        })}
+      <div className="favorites-page-container">
+        <div className="favorites-page-title">
+          <h2>Favorites</h2>
+          <h3>
+            {favorites.length === 1
+              ? `There is ${favorites.length} course saved.`
+              : `There are ${favorites.length} courses saved.`}
+          </h3>
+        </div>
+        <div className="favorites-list">
+          {favorites.map((favorite) => {
+            return (
+              <FavoriteContainer
+                courseCode={favorite}
+                setCourseCode={props.setCourseCode}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- Education_Pathways/frontend/src/pages/SearchResults/SearchResults.jsx
+++ Education_Pathways/frontend/src/pages/SearchResults/SearchResults.jsx
@@ -65,23 +65,25 @@ export default function SearchResults(props) {
   else {
     return (
       <div className="search-results-page">
-        <div className="search-results-title">
-          <h2>Search Results</h2>
-          <h3>{`${results?.courses_data?.length} result${
-            results?.courses_data?.length === 1 ? "" : "s"
-          } for "${searchTerm}"`}</h3>
-        </div>
-        <div className="search-results-list">
-          {results?.courses_data?.map((result) => {
-            return (
-              <SearchResultContainer
-                course={result}
-                searchTerm={searchTerm}
-                numberResults={results?.courses_data?.length}
-                setCourse={props.setCourse}
-              />
-            );
-          })}
+        <div className="search-results-container">
+          <div className="search-results-title">
+            <h2>Search Results</h2>
+            <h3>{`${results?.courses_data?.length} result${
+              results?.courses_data?.length === 1 ? "" : "s"
+            } for "${searchTerm}"`}</h3>
+          </div>
+          <div className="search-results-list">
+            {results?.courses_data?.map((result) => {
+              return (
+                <SearchResultContainer
+                  course={result}
+                  searchTerm={searchTerm}
+                  numberResults={results?.courses_data?.length}
+                  setCourse={props.setCourse}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     );
